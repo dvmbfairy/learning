148e7b73d1fcac1110913320d570d138e160774d
Angela May Xie <amx@utexas.edu>
2016-04-04 17:29:13 -0500

    leared how to get page titles for chrome extension

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     document.getElementById('url').innerHTML = "blah";
 
-    //chrome.tabs.getCurrent(function(tab) {
-    //    document.getElementById('url').innerHTML = tab.url;
-    //});
-
     chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
         document.getElementById('url').innerHTML = tabs[0].url;
+        document.getElementById('title').innerHTML = tabs[0].title;
     });
 });
